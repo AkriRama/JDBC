5f6cc2ad0caa6cd36ac5921dc85121c493afe61a
AkriR <114977574+AkriRama@users.noreply.github.com>
2024-12-06 15:41:04 +0700

    Update App.java

--- src/src/App.java
+++ src/src/App.java
@@ -1,12 +1,8 @@
 import controller.RoleController;
-import utils.DbConnection;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        // DbConnection connection = new DbConnection();
-        // connection.getConnection();
-
         RoleController roleController = new RoleController();
         roleController.get();
     }
