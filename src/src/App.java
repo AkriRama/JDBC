e4525de554df9fd40d3fa2c2fffc3186cf1c311b
AkriR <114977574+AkriRama@users.noreply.github.com>
2024-12-09 09:28:32 +0700

    Update App.java

--- src/src/App.java
+++ src/src/App.java
@@ -1,9 +1,9 @@
-import controller.RoleController;
+import controller.UserController;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         RoleController roleController = new RoleController();
-        roleController.get();
+        roleController.delete();
     }
 }
